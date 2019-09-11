08e725d7b06036f844f52c80b8a959b7bf18125a
ColeDB <cole.barnes@lejardinacademy.org>
2019-09-10 22:06:11 -1000

    Create Catching.py

---
+++ Catching.py
@@ -0,0 +1,16 @@
+import random
+pokemon1=("Golbat")
+combatlvl=10
+print("A wild Golbat appears!")
+pokeball=input("Do you want to catch him?")
+if(pokeball=="yes"):
+    print("You throw a ball!")
+    if(combatlvl<=15):
+        catchrate = random.randint(1, 4)
+        if(catchrate==4):
+            print(""+ pokemon1 + " Broke out")
+        else:
+            print("Zubat was caught, Congratulations!")
+
+
+
